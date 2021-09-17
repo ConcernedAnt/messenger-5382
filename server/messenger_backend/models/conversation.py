dfe8ddbd1909869ef85af09bb6b5b12097ac0328
DeeAndre Mac Andrew <34717537+ConcernedAnt@users.noreply.github.com>
2021-09-17 13:31:12 -0400

    Added find_conversations method for finding a conversation based on a list of user ids

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, Count
 from . import utils
 
 
@@ -8,13 +8,16 @@ class Conversation(utils.CustomModel):
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
-        # return conversation or None if it doesn't exist
+    # Find conversation given a list of the user ids
+    def find_conversation(user_ids):
         try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
+            query = Conversation.objects.annotate(participants_count=Count('participants')).filter(
+                participants_count=len(user_ids))
+
+            for user_id in user_ids:
+                query = query.filter(participants__userId=user_id)
+
+            return query.first()
+
         except Conversation.DoesNotExist:
             return None
